f33dd4f37ebd9d3fa49e838d73590ca7d183a74b
André Oliveira <115578007+AndreSantos09@users.noreply.github.com>
2023-09-15 11:54:44 -0300

    .

---
+++ Aulas/aula02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace aula02
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+            if (args.GetLength(0) > 0)
+            {
+                Console.WriteLine(args.GetValue(0));
+            }
+        }
+    }
+}
---
+++ Aulas/aula03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace aula03
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {   
+            byte n1 = 10; // 0 e 255
+            int var = 0; 
+            char letra = 'c';
+            float valor = 2.45f;
+            string name = "Principal";
+            Console.WriteLine("valor: ", var);
+        }
+    }
+}
